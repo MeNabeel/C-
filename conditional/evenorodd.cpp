83fb8a74881502b95139c13cc3bcc574c80bd1d2
MeNabeel <nabeelijaz559@gmail.com>
2026-03-02 16:59:38 +0500

    conditional questions now 3

---
+++ conditional/evenorodd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter Number: ";
+    cin>>num;
+    if (num%2==0){
+        cout<<"Numebr is Even"<<endl;
+    }else{
+        cout<<"Number is Odd"<<endl;
+    }
+}
---
+++ conditional/greatnum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num1, num2;
+cout<<"Enter 1st number "<<endl;
+cin>>num1;
+cout<<"Enter 2nd Number "<<endl;
+cin>>num2;
+if(num1>num2){
+    cout<<"Greater Num is "<<num1<<endl;
+}else{
+    cout<<"Greater Num is "<<num2<<endl;
+}
+}
